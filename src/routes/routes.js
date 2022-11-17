9e1f00b87379f20bfd4963ffd15b65335ec5619c
Suliman Khan <sulimank418@gmail.com>
2022-11-17 10:27:46 +0500

    backup

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const { generatePDF } = require("../middleware/pdf_generator");
+const { generatePDF } = require("../controller/pdf_generator");
 const router = new express.Router();
 const data = require("../data/data.json");
 const fs = require("fs");
 
 router.get("/", (req, res) => {
-  res.render("backup", { data });
+    res.render("backup", { data });
 });
 router.get("/generatePDF", async (req, res) => {
-  const pdf = await generatePDF({ data });
-  if (pdf) {
-    console.log("Done: pdf is created!");
-    return res.render("backup", {
-      data,
-      msg: "PDF Generated. Check Invoices folder",
-    });
-  }
-  return res.render("backup", { data, msg: "Sorry failed to generate PDF." });
+    const pdf = await generatePDF({ data });
+    if (pdf) {
+        console.log("Done: pdf is created!");
+        return res.render("backup", {
+            data,
+            msg: "PDF Generated. Check Invoices folder",
+        });
+    }
+    return res.render("backup", { data, msg: "Sorry failed to generate PDF." });
 });
 
 module.exports = router;
